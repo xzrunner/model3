0a64b6588a4324fa2f9876870d3dab324976b051
zz <zhuguang@ejoy.com>
2017-05-04 09:51:41 +0800

    [FIXED] material init

--- include/model3/Material.h
+++ include/model3/Material.h
@@ -13,7 +13,16 @@ public:
 	sm::vec3 diffuse;
 	sm::vec3 specular;
 	float    shininess;
-	int      tex;	
+	int      tex;
+
+public:
+	Material() 
+		: ambient(0.04f, 0.04f, 0.04f)
+		, diffuse(1, 1, 1)
+		, specular(1, 1, 1)
+		, shininess(50)
+		, tex(-1)
+	{}
 
 }; // Material
 
