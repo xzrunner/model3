97c3e3f31266d8c6f2c61374b5a4d6f5ccb58667
zz <zhuguang@ejoy.com>
2018-01-01 19:44:53 +0800

    [FIXED] ctx's proj mat and viewport

--- include/model3/RenderContext.h
+++ include/model3/RenderContext.h
@@ -8,13 +8,23 @@ namespace m3
 class RenderContext
 {
 public:
+	RenderContext();
 
 	void SetModelView(const sm::mat4& mt);
-	
+	void SetProjection(const sm::mat4& mt);
+	void SetScreen(int width, int height);
+
+	void UpdateMVP() const;
 	void UpdateModelView() const;
+	void UpdateProjection() const;
+	void UpdateViewport() const;
 
 private:
 	sm::mat4 m_mv_mat;
+	sm::mat4 m_proj_mat;
+
+	int m_screen_width;
+	int m_screen_height;
 
 }; // RenderContext
 
--- source/RenderContext.cpp
+++ source/RenderContext.cpp
@@ -1,9 +1,17 @@
 #include "model3/RenderContext.h"
 
 #include <shaderlab/SubjectMVP3.h>
+#include <shaderlab/ShaderMgr.h>
+#include <unirender/RenderContext.h>
 
 namespace m3
 {
+
+RenderContext::RenderContext()
+	: m_screen_width(0)
+	, m_screen_height(0)
+{
+}
 
 void RenderContext::SetModelView(const sm::mat4& mt)
 {
@@ -16,9 +24,49 @@ void RenderContext::SetModelView(const sm::mat4& mt)
 	UpdateModelView();
 }
 
+void RenderContext::SetProjection(const sm::mat4& mt)
+{
+	if (m_proj_mat == mt) {
+		return;
+	}
+
+	m_proj_mat = mt;
+
+	UpdateProjection();
+}
+
+void RenderContext::SetScreen(int width, int height)
+{
+	m_screen_width = width;
+	m_screen_height = height;
+
+	UpdateViewport();
+}
+
+void RenderContext::UpdateMVP() const
+{
+	UpdateModelView();
+	UpdateProjection();
+}
+
 void RenderContext::UpdateModelView() const
 {
 	sl::SubjectMVP3::Instance()->NotifyModelview(m_mv_mat);
+}
+
+void RenderContext::UpdateProjection() const
+{
+	sl::SubjectMVP3::Instance()->NotifyProjection(m_proj_mat);
+}
+
+void RenderContext::UpdateViewport() const
+{
+	if (m_screen_width == 0 && m_screen_height == 0) {
+		return;
+	}
+
+	sl::ShaderMgr::Instance()->GetContext()->SetViewport(
+		0, 0, m_screen_width, m_screen_height);
 }
 
 }
