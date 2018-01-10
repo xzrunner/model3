4210cc4197372f2afd8adf59c364243ca2889057
zz <zhuguang@ejoy.com>
2018-01-10 22:53:01 +0800

    [ADDED] transform vertices: pos and normal

--- source/ObjectModel.cpp
+++ source/ObjectModel.cpp
@@ -22,6 +22,9 @@ void ObjectModel::Draw(const RenderParams& rp) const
 	sl::ShaderMgr* mgr = sl::ShaderMgr::Instance();
 	mgr->SetShader(sl::MODEL3);
 	sl::Model3Shader* shader = static_cast<sl::Model3Shader*>(mgr->GetShader());
+
+	shader->SetLightPosition(sm::vec3(0.25f, 0.25f, 1));
+
 	auto& meshes = m_model->GetAllMeshes();
 	for (auto& mesh : meshes)
 	{
@@ -29,15 +32,32 @@ void ObjectModel::Draw(const RenderParams& rp) const
 		int tex_id = n3::ResourceAPI::GetTexID(material.texture);
 		shader->SetMaterial(sl::Model3Shader::Material(material.ambient, 
 			material.diffuse, material.specular, material.shininess, tex_id));
-		shader->SetLightPosition(sm::vec3(0.25f, 0.25f, 1));
-		shader->SetNormalMatrix(rp.mt);
 
 		int vertex_type = mesh->GetVertexType();
-		bool normal = vertex_type & n3::VERTEX_FLAG_NORMALS;
-		bool texcoords = vertex_type & n3::VERTEX_FLAG_TEXCOORDS;
-		auto& vertices = mesh->GetVertices();
+		bool has_normal = vertex_type & n3::VERTEX_FLAG_NORMALS;
+		bool has_texcoord = vertex_type & n3::VERTEX_FLAG_TEXCOORDS;
+
+		// transform vertices
+		auto vertices = mesh->GetVertices();
+		int stride = 3;
+		if (has_normal) {
+			stride += 3;
+		}
+		if (has_texcoord) {
+			stride += 2;
+		}
+		assert(vertices.size() % stride == 0);
+		for (int i = 0, n = vertices.size(); i < n; i += stride) {
+			sm::vec3 pos = rp.mt * sm::vec3(vertices[i], vertices[i + 1], vertices[i + 2]);
+			memcpy(&vertices[i], &pos.xyz[0], sizeof(float) * 3);
+			if (has_normal) {
+				sm::vec3 normal = rp.mt * sm::vec3(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+				memcpy(&vertices[i + 3], &normal.xyz[0], sizeof(float) * 3);
+			}
+		}
+
 		auto& indieces = mesh->GetIndices();
-		shader->Draw(&vertices[0], vertices.size(), &indieces[0], indieces.size(), normal, texcoords);
+		shader->Draw(&vertices[0], vertices.size(), &indieces[0], indieces.size(), has_normal, has_texcoord);
 	}
 }
 
