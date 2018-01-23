b565d653a836ea057a3f002420ce347e2701bb7b
zz <zhuguang@ejoy.com>
2018-01-24 06:14:16 +0800

    [CHANGED] add some api

--- include/node3/CompTransform.h
+++ include/node3/CompTransform.h
@@ -17,17 +17,24 @@ public:
 		, m_scale(1, 1, 1) 
 	{}
 
-	void SetPosition(const sm::vec3& pos) {
-		m_position = pos;
-	}
+	const sm::vec3& GetPosition() const { return m_position; }
+	const sm::Quaternion& GetAngle() const { return m_angle; }
+	const sm::vec3& GetScale() const { return m_scale; }
 
-	sm::mat4 GetTransformMat() const 
+	sm::mat4 GetTransformMat() const
 	{
 		auto mt_scale = sm::mat4::Scaled(m_scale.x, m_scale.y, m_scale.z);
 		auto mt_rot = sm::mat4(m_angle);
 		auto mt_trans = sm::mat4::Translated(m_position.x, m_position.y, m_position.z);
-		return mt_trans * mt_rot * mt_scale;
+		return mt_scale * mt_rot * mt_trans;
 	}
+
+	void SetPosition(const sm::vec3& pos) { m_position = pos; }
+	void SetAngle(const sm::Quaternion& angle) { m_angle = angle; }
+	void SetScale(const sm::vec3& scale) { m_scale = scale; }
+
+	void Translate(const sm::vec3& offset) { m_position += offset; }
+	void Rotate(const sm::Quaternion& delta) { m_angle = delta.Rotated(m_angle); }
 
 private:
 	sm::vec3       m_position;
